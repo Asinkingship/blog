a03366558ed00f2d13174dd6473b058df7f4be76
root <root@DESKTOP-0RSBRDK>
2024-08-13 23:08:09 -0400

    session 4 added

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    'accounts',
+    'posts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView
+)
+from .models import Post
+# from django.urls import reverse
 
-# Create your views here.
+
+class PostListView(ListView):                           # scan
+    template_name = 'posts/list.html'
+    model = Post
+
+class PostDetailView(DetailView):                       # read single
+    template_name = 'posts/detail.html'
+    model = Post
+
+class PostCreateView(CreateView):                       # create new records
+    template_name = 'posts/new.html'
+    model = Post
+    fields = ['title', 'subtitle', 'body']
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    success_url = reverse_lazy("list")
+    # success_url = reverse("list")
