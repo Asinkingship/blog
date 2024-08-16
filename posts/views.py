15b0b69a5539ee60ef3879fd92c4b7d7ac6eb769
root <root@DESKTOP-0RSBRDK>
2024-08-15 22:55:44 -0400

    update to app with all info from 113 class

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,3 @@
-from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
@@ -6,6 +5,11 @@ from django.views.generic import (
     DeleteView,
     UpdateView
 )
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.urls import reverse_lazy
 from .models import Post
 # from django.urls import reverse
 
@@ -18,18 +22,30 @@ class PostDetailView(DetailView):                       # read single
     template_name = 'posts/detail.html'
     model = Post
 
-class PostCreateView(CreateView):                       # create new records
+class PostCreateView(LoginRequiredMixin, CreateView):                       # create new records
     template_name = 'posts/new.html'
     model = Post
-    fields = ['title', 'subtitle', 'body', 'author', 'status']
+    fields = ['title', 'subtitle', 'body', 'status']
 
-class PostDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "posts/delete.html"
     model = Post
     success_url = reverse_lazy("list")
 
-class PostUpdateView(UpdateView):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     model = Post
     fields = ['title', 'subtitle', 'body', 'status']
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
     
